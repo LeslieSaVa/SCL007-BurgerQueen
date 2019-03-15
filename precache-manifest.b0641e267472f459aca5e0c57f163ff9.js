efb2083c5ac86c14dbb36cb5024350f6dd14dad3
LeslieSaVa <leslie.sabattin@usach.cl>
2019-03-14 23:02:49 -0300

    Updates

---
+++ precache-manifest.b0641e267472f459aca5e0c57f163ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36701f6ffdbd4ab4b6e0",
+    "url": "/SCL007-BurguerQueen/static/css/main.7f3668b5.chunk.css"
+  },
+  {
+    "revision": "36701f6ffdbd4ab4b6e0",
+    "url": "/SCL007-BurguerQueen/static/js/main.36701f6f.chunk.js"
+  },
+  {
+    "revision": "224c51bbe036ba331df2",
+    "url": "/SCL007-BurguerQueen/static/js/runtime~main.224c51bb.js"
+  },
+  {
+    "revision": "c67340c964074c20f640",
+    "url": "/SCL007-BurguerQueen/static/js/2.c67340c9.chunk.js"
+  },
+  {
+    "revision": "aec3d3837b49194892b56f7a1ceb08f7",
+    "url": "/SCL007-BurguerQueen/index.html"
+  }
+];
