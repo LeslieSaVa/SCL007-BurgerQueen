8d9c3224ca39baacab9e2ecb764cb699ab860a61
LeslieSaVa <leslie.sabattin@usach.cl>
2019-03-29 14:42:46 -0300

    Updates

--- precache-manifest.8f8d3338e3318e3395c05519ef83673a.js
+++ precache-manifest.8f8d3338e3318e3395c05519ef83673a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4f4ec1bece0ffc39888e",
+    "revision": "1d6fe95c89b9687b3494",
     "url": "/SCL007-BurgerQueen/static/css/main.238f97bd.chunk.css"
   },
   {
-    "revision": "4f4ec1bece0ffc39888e",
-    "url": "/SCL007-BurgerQueen/static/js/main.4f4ec1be.chunk.js"
+    "revision": "1d6fe95c89b9687b3494",
+    "url": "/SCL007-BurgerQueen/static/js/main.1d6fe95c.chunk.js"
   },
   {
     "revision": "ef0ee4a49c678152c9da",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/SCL007-BurgerQueen/static/js/2.ae3c6a05.chunk.js"
   },
   {
-    "revision": "48f4db390f500646a4a30baef104c74e",
+    "revision": "4c66665b0d62e10ec33e00fac1278b0b",
     "url": "/SCL007-BurgerQueen/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SCL007-BurgerQueen/precache-manifest.ecbe17359f9342452ce749122eb82769.js"
+  "/SCL007-BurgerQueen/precache-manifest.8f8d3338e3318e3395c05519ef83673a.js"
 );
 
 workbox.clientsClaim();
