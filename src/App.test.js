d2109ebc9d9e1ed3967423b7e883aa113f8bfae5
LeslieSaVa <leslie.sabattin@usach.cl>
2019-04-03 15:47:59 -0300

    primer test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import menu from './data/menu.json'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// import InputUser from './post/input';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App menu={menu}/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('debe retornar leslie', () => {  
+  const appUserComponent = Enzyme.mount(<App menu={menu} />);//nombre del componente donde esta el input que queremos testear
+  const buttonComponent = appUserComponent.find("InputUser").find('button');
+  appUserComponent.find("InputUser").find("input").simulate('change', { target: {value: 'leslie'} });
+  buttonComponent.filter('.btn-save').simulate('click');
+  expect(appUserComponent.state().name).toEqual('leslie');
 });
--- src/post/header.js
+++ src/post/header.js
@@ -7,7 +7,7 @@ function Header (props) {
             <div className="header">
                 <p>
                     {/* <a className="btn-waitress col s6 offset-s6 waves-effect  btn-large">Garzones</a> */}
-                    Burguer Queen Pedidos
+                    Burger Queen Pedidos
                     {/* <a className="btn-kitchen col s6 offset-s6 waves-effect  btn-large">Cocina</a> */}
                 </p>
 
